bc7fad5b744f9cada3086a04c00c116006c2b931
Boogie <spiertzjc@icloud.com>
2026-05-13 18:32:11 +1200

    Polish RepoRunner setup screen

--- artifacts/reporunner/electron/main.ts
+++ artifacts/reporunner/electron/main.ts
@@ -27,7 +27,6 @@ function createWindow() {
 
   if (isDev) {
     mainWindow.loadURL("http://localhost:5173");
-    mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, "../dist/public/index.html"));
   }
